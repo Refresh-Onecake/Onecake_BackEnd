ee4243063589495415df367fea6c91508db17187
yubin <dbqls2821@gmail.com>
2022-05-18 23:47:38 +0900

    initial setting

---
+++ src/main/java/refresh/onecake/OnecakeApplication.java
@@ -0,0 +1,13 @@
+package refresh.onecake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnecakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnecakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/refresh/onecake/OnecakeApplicationTests.java
@@ -0,0 +1,13 @@
+package refresh.onecake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnecakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
