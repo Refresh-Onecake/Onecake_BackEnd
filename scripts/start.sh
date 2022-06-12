3551eaffa184c037d112fae9b98fc7b949214fb5
Yubin <69676101+shinyubin989@users.noreply.github.com>
2022-06-12 14:17:35 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,7 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/build/libs/onecake-0.0.1-SNAPSHOT.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
