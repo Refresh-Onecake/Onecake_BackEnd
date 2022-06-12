14da3df780ca8c1f67ea65c4d84bec2739bf7ace
yubin <dbqls2821@gmail.com>
2022-06-12 13:36:49 +0900

    chore: java version 11 to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allOpen {
 
 group = "refresh"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
@@ -66,7 +66,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
 
