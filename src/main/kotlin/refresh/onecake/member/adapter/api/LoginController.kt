0fbed9330c3ec93dfd2c7451f88e17d689b76a31
yubin <dbqls2821@gmail.com>
2022-06-13 19:07:01 +0900

    feat: add swagger note

--- src/main/kotlin/refresh/onecake/member/adapter/api/LoginController.kt
+++ src/main/kotlin/refresh/onecake/member/adapter/api/LoginController.kt
@@ -1,5 +1,6 @@
 package refresh.onecake.member.adapter.api
 
+import io.swagger.annotations.ApiOperation
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import refresh.onecake.member.adapter.api.SwaggerNotes.Companion.LOGIN_MEMO
+import refresh.onecake.member.adapter.api.SwaggerNotes.Companion.REISSUE_MEMO
+import refresh.onecake.member.adapter.api.SwaggerNotes.Companion.SIGNUP_MEMO
 import refresh.onecake.member.adapter.api.dto.*
 import refresh.onecake.member.application.LoginService
 
@@ -17,6 +21,7 @@ class LoginController (
     private val loginService: LoginService
 ){
     @Operation(summary = "회원가입")
+    @ApiOperation(value = "회원가입", notes = SIGNUP_MEMO)
     @PostMapping("/signup")
     fun signup(@RequestBody signUpRequestDto: SignUpRequestDto): ResponseEntity<SignUpResponseDto> {
         val signUpResponseDto:SignUpResponseDto = loginService.signup(signUpRequestDto)
@@ -28,11 +33,14 @@ class LoginController (
     }
 
     @Operation(summary = "로그인")
+    @ApiOperation(value = "로그인", notes = LOGIN_MEMO)
     @PostMapping("/login")
     fun login(@RequestBody loginRequestDto: LoginRequestDto): ResponseEntity<TokenDto> {
         return ApiResponse.success(HttpStatus.OK,  loginService.login(loginRequestDto))
     }
 
+    @Operation(summary = "토큰 재발급")
+    @ApiOperation(value = "토큰 재발급", notes = REISSUE_MEMO)
     @PostMapping("/reissue")
     fun reissue(@RequestBody tokenRequestDto: TokenRequestDto): ResponseEntity<TokenDto> {
         val tokenDto:TokenDto? = loginService.reissue(tokenRequestDto)
