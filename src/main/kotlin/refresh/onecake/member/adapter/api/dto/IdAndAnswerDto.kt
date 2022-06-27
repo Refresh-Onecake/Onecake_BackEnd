abf36e20aec76c069b65611cf709e05015da54b9
yubin <dbqls2821@gmail.com>
2022-06-27 15:14:10 +0900

    chore: delete unused DTO

--- src/main/kotlin/refresh/onecake/member/adapter/api/dto/IdAndAnswerDto.kt
+++
@@ -1,6 +0,0 @@
-package refresh.onecake.member.adapter.api.dto
-
-data class IdAndAnswerDto(
-    var id: Long,
-    var answer: String
-)
--- src/main/kotlin/refresh/onecake/member/adapter/api/dto/IdAndQuestionDto.kt
+++
@@ -1,6 +0,0 @@
-package refresh.onecake.member.adapter.api.dto
-
-data class IdAndQuestionDto (
-    var id: Long,
-    var question: String
-)
