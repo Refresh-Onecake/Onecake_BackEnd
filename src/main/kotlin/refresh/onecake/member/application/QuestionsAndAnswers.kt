feb61161e181410d64dc42797b139ca104f80152
yubin <dbqls2821@gmail.com>
2022-07-29 13:40:38 +0900

    chore: consumer can only see cakeInputs in order history

--- src/main/kotlin/refresh/onecake/member/application/QuestionsAndAnswers.kt
+++ src/main/kotlin/refresh/onecake/member/application/QuestionsAndAnswers.kt
@@ -1,24 +1,18 @@
 package refresh.onecake.member.application
 
-import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 import refresh.onecake.member.domain.common.OrderSheetRepository
-import refresh.onecake.member.domain.common.QuestionRepository
 
 @Component
 class QuestionsAndAnswers(
-    private val orderSheetRepository: OrderSheetRepository,
-    private val questionRepository: QuestionRepository
+    private val orderSheetRepository: OrderSheetRepository
 ) {
 
     fun getQuestionsAndAnswers(orderHistoryId: Long): List<String> {
-        val orderSheet = orderSheetRepository.findAllByOrderId(orderHistoryId)
-        val answers = orderSheet?.map { it.answer }
-        val questionIds = orderSheet?.map { it.questionId }
-
-        var forms: MutableList<String> = mutableListOf()
-        for (i in questionIds?.indices!!) {
-            forms.add(i, questionRepository.findQuestionById(questionIds[i]).question + " : " + answers?.get(i))
+        val questionAndAnswer = orderSheetRepository.getBundleOfQuestionAndAnswerWhoseConsumerInputIsFalse(orderHistoryId)
+        var forms = mutableListOf<String>()
+        for (i in questionAndAnswer.indices) {
+            forms.add(i, questionAndAnswer[i].question + " : " +questionAndAnswer[i].answer)
         }
         return forms
     }
