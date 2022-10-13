57633813aa82251900d275cb61215118057f0290
yubin <dbqls2821@gmail.com>
2022-10-13 23:38:51 +0900

    feat: consumer can see likedStores in mypage

--- src/main/kotlin/refresh/onecake/storelike/adapter/api/StoreLikeController.kt
+++ src/main/kotlin/refresh/onecake/storelike/adapter/api/StoreLikeController.kt
@@ -3,12 +3,10 @@ package refresh.onecake.storelike.adapter.api
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import refresh.onecake.response.adapter.dto.DefaultResponseDto
 import refresh.onecake.response.adapter.api.ApiResponse
+import refresh.onecake.storelike.adapter.dto.LikedStoreWithReviewNum
 import refresh.onecake.storelike.application.StoreLikeService
 
 @RestController
@@ -23,6 +21,15 @@ class StoreLikeController (
         return ApiResponse.success(
             HttpStatus.OK,
             storeLikeService.pushStoreLike(storeId)
+        )
+    }
+
+    @Operation(summary = "소비자 - 마이페이지 - 좋아요 누른 가게 ")
+    @GetMapping("consumer/mypage/like/store")
+    fun getLikedStoreList(): ResponseEntity<List<LikedStoreWithReviewNum>> {
+        return ApiResponse.success(
+            HttpStatus.OK,
+            storeLikeService.getLikedStoreList()
         )
     }
 }
